992d7afa178866186a43f3092907630cf40222c1
Ihor Onyshchuk <ihor-onyshchuk@MacBook-Pro.local>
2023-08-24 16:08:41 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
